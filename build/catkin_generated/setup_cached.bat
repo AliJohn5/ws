7cd8f25ee8d5f6c6911f934d9da540b687eea571
AliJohn5 <aliy65154@gmial.com>
2023-04-14 22:59:01 +0100

    create ROS pkg

---
+++ build/catkin_generated/setup_cached.bat
@@ -0,0 +1,16 @@
+@echo off
+REM generated from catkin/python/catkin/environment_cache.py
+
+REM based on a snapshot of the environment before and after calling the setup script
+REM it emulates the modifications of the setup script without recurring computations
+
+REM new environment variables
+set LD_LIBRARY_PATH=c:\opt\ros\noetic\x64\lib
+set PYTHONHOME=C:\opt\ros\noetic\x64\
+
+REM modified environment variables
+set CMAKE_PREFIX_PATH=C:\opt\ros\noetic\x64\tools\vcpkg\installed\x64-windows;C:/arms/ws/devel;c:\opt\ros\noetic\x64
+set PATH=C:\opt\ros\noetic\x64\tools\vcpkg\installed\x64-windows\bin;C:\opt\ros\noetic\x64\tools\vcpkg;c:\opt\ros\noetic\x64\bin;c:\opt\ros\noetic\x64\lib;C:\opt\ros\noetic\x64\;C:\opt\ros\noetic\x64\Scripts;c:\opt\ros\noetic\x64\Scripts;c:\opt\ros\noetic\x64\;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.35.32215\bin\HostX64\x64;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\VC\VCPackages;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer;C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\bin\Roslyn;C:\Program Files\Microsoft Visual Studio\2022\Community\Team Tools\Performance Tools\x64;C:\Program Files\Microsoft Visual Studio\2022\Community\Team Tools\Performance Tools;C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\\x64;C:\Program Files (x86)\Windows Kits\10\bin\\x64;C:\Program Files\Microsoft Visual Studio\2022\Community\\MSBuild\Current\Bin\amd64;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Users\Ali_John\AppData\Local\Microsoft\WindowsApps;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\VC\Linux\bin\ConnectionManagerExe
+set ROS_ETC_DIR=c:\opt\ros\noetic\x64/etc/ros
+set ROS_PACKAGE_PATH=C:/arms/ws/src;%ROS_PACKAGE_PATH%
+set ROS_ROOT=c:\opt\ros\noetic\x64/share/ros
